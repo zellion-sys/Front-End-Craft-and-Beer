72769a328ec011754ff46c0554d9c9ceb70647cb
EduardoDiaz <eduardotressa@gmail.com>
2025-11-26 21:51:14 -0300

    commit

---
+++ js/# .py
@@ -0,0 +1,6 @@
+# .env (¡No lo compartas!)
+# Reemplaza <usuario>:<contraseña>@<cluster> con tu cadena real de Atlas
+MONGO_URL="mongodb+srv://maxdiaz120_db_user:xuUmQ9Bwo0pOtt0x@cluster0.nj4nrpp.mongodb.net/?appName=Cluster0"
+DATABASE_NAME="craft_beer_db"
+# Cambia esta clave secreta a una cadena larga y aleatoria
+SECRET_KEY="xuUmQ9Bwo0pOtt0x"
